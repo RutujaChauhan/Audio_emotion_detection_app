8073725441911849e89a67f809458f1258d872f9
Rutuja Chauhan <95647723+RutujaChauhan@users.noreply.github.com>
2023-09-14 14:54:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,98 @@
+import streamlit as st
+import pickle
+import librosa
+import numpy as np
+import pandas as pd
+import pyaudio
+import soundfile as sf
+from PIL import Image
+
+# Load the trained model
+
+audio_model = pd.read_pickle("audio_detectionModel.pkl")
+image = Image.open(
+    "C:\\Users\\rutuj\\OneDrive\\Desktop\\Desktop\\audio_processing\\app1\\image.png",
+)
+
+
+# Define the emotion labels based on your encoding
+emotion_labels = [
+    "Angry",
+    "Disgust",
+    "Fear",
+    "Happy",
+    "Neutral",
+    "Pleasant surprised",
+    "Sad",
+]
+
+
+# Function to extract audio features
+def extract_features(audio_file):
+    try:
+        audio_data, _ = librosa.load(audio_file, duration=3, offset=0.5)
+        mfccs = librosa.feature.mfcc(y=audio_data, sr=_, n_mfcc=40)
+        return mfccs
+    except Exception as e:
+        st.error(f"Error processing audio: {str(e)}")
+        return None
+
+
+# Function to predict emotion
+def predict_emotion(audio_features):
+    try:
+        audio_features = np.mean(audio_features, axis=1)
+        audio_features = audio_features.reshape(1, -1)
+        prediction = audio_model.predict(audio_features)
+        # return prediction[0]
+        predicted_emotion = emotion_labels[np.argmax(prediction)]
+        return predicted_emotion
+    except Exception as e:
+        st.error(f"Error predicting emotion: {str(e)}")
+        return None
+
+
+# Streamlit UI
+
+st.title("Audio Emotion Detection App ")
+st.image(
+    image,
+    width=300,
+)
+st.write("Record or Upload an audio file to detect the emotion.")
+
+
+# Option to record audio
+recorded_audio = None
+recording = False
+if st.button("Record Audio"):
+    recording = not recording
+
+if recording:
+    st.write("Recording... (Click again to stop)")
+
+if not recording and recorded_audio is not None:
+    with open("temp_audio.wav", "wb") as audio_file:
+        audio_file.write(recorded_audio)
+        st.audio(audio_file, format="mp3/wav", start_time=0)
+
+    st.write("Processing...")
+    audio_features = extract_features("temp_audio.wav")
+
+    if audio_features is not None:
+        emotion = predict_emotion(audio_features)
+        if emotion is not None:
+            st.success(f"Emotion Detected: {emotion}")
+
+uploaded_file = st.file_uploader("Choose an audio file...", type=["wav", "mp3"])
+
+if uploaded_file:
+    st.audio(uploaded_file, format="mp3/wav", start_time=0)
+
+    if st.button("Detect Emotion"):
+        st.write("Processing...")
+        audio_features = extract_features(uploaded_file)
+        if audio_features is not None:
+            emotion = predict_emotion(audio_features)
+            if emotion is not None:
+                st.success(f"Emotion Detected: {emotion}")
